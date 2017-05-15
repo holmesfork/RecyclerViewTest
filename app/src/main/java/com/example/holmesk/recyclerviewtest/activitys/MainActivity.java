4ea4b1cc932d61bb439056659004082c28cc4fef
holmesfork <1976696499@qq.com>
2017-05-15 20:14:58 +0800

    sss

--- app/src/main/java/com/example/holmesk/recyclerviewtest/activitys/MainActivity.java
+++ app/src/main/java/com/example/holmesk/recyclerviewtest/activitys/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         initData();
         initView();
         initEvent();
-
+        //sadlsakdjaslkdjasl
     }
 
     private void initData() {
